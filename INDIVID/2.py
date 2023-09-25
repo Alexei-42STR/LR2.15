e05e836108387c0329cf4944be19eb94ad9ba905
Alexey Voloshin <125445350+Alexei-42STR@users.noreply.github.com>
2023-09-25 03:11:53 +0300

    Update 2.py

--- INDIVID/2.py
+++ INDIVID/2.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# 8. В данном упражнении вы должны написать программу, которая будет находить самое
-# длинное слово в файле. В качестве результата программа должна выводить на экран длину
-# самого длинного слова и все слова такой длины. Для простоты принимайте за значимые
-# буквы любые непробельные символы, включая цифры и знаки препинания.
-
+# 7. Разработайте программу, которая будет показывать слово (или слова), чаще остальных встречающееся в текстовом файле. Сначала пользователь должен ввести имя файла для обработки. После этого вы должны открыть файл и проанализировать его построчно, разделив при этом строки по словам и исключив из них пробелы и знаки препинания. Также при подсчете частоты появления слов в файле вам стоит игнорировать регистры.
 if __name__ == "__main__":
+    
+s = inf.read().replace('\n', ' ').lower().split()
+words = {}
+words[s[0]] = 1
+k = 0
 
-    def longest_word(filename):
-        with open("2txt.txt", "r", encoding="utf-8") as infile:
-            words = infile.read().split()
-        max_len = len(max(words, key=len))
-        return [word for word in words if len(word) == max_len]
+for i in range (0,len(s)-1):
+    if s[i] in words:
+        k+=1
+        words[s[i]] = k
+    else:
+        words[s[i]] = 1
 
-    print(longest_word("1.txt"))
+word = list(words.keys())
+word_count = list(words.values())
+max_word_count = 0
+min_word = ''
+
+for i in range(len(word_count)-1):
+    if  word_count[i] > max_word_count:
+        max_word_count = word_count[i]
+        min_word = word[i]
+    elif word_count[i] == max_word_count:
+        if word[i] < min_word:
+            min_word = word[i]
+
+print(min_word,max_word_count)
